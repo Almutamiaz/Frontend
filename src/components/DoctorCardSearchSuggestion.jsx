da48e82f505e72cbaf98cdc0c5d5ccdbd19f1b91
mutazDev <4mutaz@gmail.com>
2025-07-01 15:23:06 +0300

    Update DoctorCardSearchSuggestion.jsx

--- src/components/DoctorCardSearchSuggestion.jsx
+++ src/components/DoctorCardSearchSuggestion.jsx
@@ -3,12 +3,17 @@ import Link from "next/link";
 import DummyDoctorImage from "@/assets/images/DummyDoctorImage.jpg";
 import { useTranslations } from "next-intl";
 
-const DoctorCardSearchSuggestion = ({ img, name, specialization, href = "" }) => {
+const DoctorCardSearchSuggestion = ({
+  img,
+  name,
+  specialization,
+  href = "",
+}) => {
   const t = useTranslations();
 
   return (
     <div className="flex gap-3 items-center border-b-[0.5px] border-[var(--neutral-200)] pb-[9px]">
-      <div className="w-[44px] h-[44px] overflow-hidden">
+      <div className="w-[44px] h-[44px] overflow-hidden flex-shrink-0">
         <Image
           className="w-full h-full rounded-[50%] object-cover"
           src={img}
