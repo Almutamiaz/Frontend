159e3c24946169a708e79d31728bd91edb5b9e40
mutazDev <4mutaz@gmail.com>
2025-09-04 19:41:12 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const BASE_URL_WithOutSite =
   process.env.NEXT_PUBLIC_BASE_URL_WITHOUT_SITE;
-export const ORIGINAL_BASE_URLBASE_URL =
+export const ORIGINAL_BASE_URL =
   process.env.NEXT_PUBLIC_ORIGINAL_BASE_URL;
 export const PRODUCTION_URL = "https://hakeem.com.sa";
